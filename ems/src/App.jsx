59e60e4232d9b3b072b85cdde02f1c30c04600e8
yadavchhhama <119515947+yadavchhhama@users.noreply.github.com>
2025-03-20 06:48:39 +0000

    commit

---
+++ ems/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import Login from './components/Auth/Login';
+const App = () => {
+  return (
+    <div> 
+      <Login />
+    </div>
+  )
+}
+
+export default App
